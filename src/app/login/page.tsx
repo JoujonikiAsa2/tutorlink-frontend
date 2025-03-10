0054e7961e876219303110fd4a11eb040f03c9c7
JoujonikiAsa2 <nibeditaroy582@gmail.com>
2025-03-10 15:40:22 +0600

    added hero section and update login and register's background

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,9 +1,22 @@
 import LoginForm from "@/components/modules/auth/login/LoginForm";
 import React from "react";
-import styles from "./Login.module.css";
+
+export const metadata = {
+  title: "Login",
+  description: "Login as a tutor or student",
+};
+
 const LoginPage = () => {
   return (
-    <div className={styles.loginContainer}>
+    <div
+    style={{
+      backgroundImage: "url('/bg-01.jpg')",
+      backgroundSize: "cover",
+      backgroundPosition: "center",
+      backgroundRepeat: "no-repeat",
+    }}
+    className="flex flex-col min-h-[100vh] items-center justify-center"
+  >
       <LoginForm />
     </div>
   );
--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -16,14 +16,14 @@ const buttonVariants = cva(
         outline:
           "border border-input bg-background shadow-xs hover:bg-accent hover:text-accent-foreground",
         secondary:
-          "bg-secondary text-secondary-foreground shadow-xs hover:bg-secondary/80",
+          "bg-secondary text-secondary-foreground shadow-xs hover:bg-secondary/80 text-white",
         ghost: "hover:bg-accent hover:text-accent-foreground",
         link: "text-primary underline-offset-4 hover:underline",
       },
       size: {
-        default: "h-9 px-4 py-2 has-[>svg]:px-3",
-        sm: "h-8 rounded-md gap-1.5 px-3 has-[>svg]:px-2.5",
-        lg: "h-10 rounded-md px-6 has-[>svg]:px-4",
+        default: "h-9 w-fit px-4 py-2 has-[>svg]:px-3",
+        sm: "h-8 w-fit rounded-md gap-1.5 px-3 has-[>svg]:px-2.5",
+        lg: "h-10 w-fit rounded-md px-6 has-[>svg]:px-4",
         icon: "size-9",
       },
     },
